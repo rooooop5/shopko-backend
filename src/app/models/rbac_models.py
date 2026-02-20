79580aa3a22f323cd5851f0bd5c36323fcf79308
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-02-20 13:13:41 +0530

    Now added all permissions

--- src/app/models/rbac_models.py
+++ src/app/models/rbac_models.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from sqlmodel import SQLModel,Field,Relationship
+from sqlmodel import SQLModel, Field, Relationship
 from typing import Annotated
+
 
 class RolesEnum(str, Enum):
     MODERATOR = "moderator"
@@ -9,37 +10,64 @@ class RolesEnum(str, Enum):
 
 
 class PermissionsEnum(Enum):
-    viewProducts = "view_products"
-    listProducts = "list_products"
-    getProduct = "get_product"
-    placeOrder = "place_order"
-    cancelOrder = "cancel_order"
-    listOwnOrders = "list_own_orders"
-    viewOwnOrder = "view_own_order"
+    viewProducts = "view_products"  # customer,seller,moderator
+    listProduct = "list_product"  # seller
+    getProduct = "get_product"  # customer,seller,moderator
+    placeOrder = "place_order"  # customer
+    viewOrder = "view_order"  # customer,seller
+    listOrders = "list_orders"  # customer,seller
+    cancelOrder = "cancel_order"  # customer,seller
+    updateProduct = "update_product"  # seller
+    viewSeller = "view_seller"  # moderator
+    removeSeller = "remove_seller"  # moderator
+    viewCustomer = "view_customer"  # moderator
+    removeCustomer = "remove_customer"  # moderator
+    removeProduct = "remove_product"  # seller,moderator
 
-class RolePermissions(SQLModel,table=True):
-    role:Annotated[int,Field(foreign_key="roles.id",primary_key=True)]
-    permission:Annotated[int,Field(foreign_key="permissions.id",primary_key=True)]
 
-role_permissions_mapping={
-    RolesEnum.MODERATOR:[],
-    RolesEnum.CUSTOMER:[PermissionsEnum.viewProducts,PermissionsEnum.getProduct,PermissionsEnum.viewOwnOrder,PermissionsEnum.listOwnOrders,PermissionsEnum.cancelOrder],
-    RolesEnum.SELLER:[PermissionsEnum.listProducts]
+class RolePermissions(SQLModel, table=True):
+    role: Annotated[int, Field(foreign_key="roles.id", primary_key=True)]
+    permission: Annotated[int, Field(foreign_key="permissions.id", primary_key=True)]
+
+
+role_permissions_mapping = {
+    RolesEnum.MODERATOR: [
+        PermissionsEnum.viewProducts,
+        PermissionsEnum.getProduct,
+        PermissionsEnum.viewSeller,
+        PermissionsEnum.removeSeller,
+        PermissionsEnum.viewCustomer,
+        PermissionsEnum.removeCustomer,
+        PermissionsEnum.removeProduct,
+    ],
+    RolesEnum.CUSTOMER: [
+        PermissionsEnum.viewProducts,
+        PermissionsEnum.getProduct,
+        PermissionsEnum.placeOrder,
+        PermissionsEnum.viewOrder,
+        PermissionsEnum.listOrders,
+        PermissionsEnum.cancelOrder,
+    ],
+    RolesEnum.SELLER: [
+        PermissionsEnum.viewProducts,
+        PermissionsEnum.listProduct,
+        PermissionsEnum.getProduct,
+        PermissionsEnum.viewOrder,
+        PermissionsEnum.listOrders,
+        PermissionsEnum.cancelOrder,
+        PermissionsEnum.updateProduct,
+        PermissionsEnum.removeProduct,
+    ],
 }
-
-
-
 
 
 class Permissions(SQLModel, table=True):
     id: Annotated[int, Field(primary_key=True)]
     permission: Annotated[str, Field(unique=True)]
-    roles:list["Roles"]=Relationship(back_populates="permissions",link_model=RolePermissions)
-
+    roles: list["Roles"] = Relationship(back_populates="permissions", link_model=RolePermissions)
 
 
 class Roles(SQLModel, table=True):
     id: Annotated[int, Field(primary_key=True)]
     role: Annotated[str, Field(unique=True)]
-    permissions:list["Permissions"]=Relationship(back_populates="roles",link_model=RolePermissions)
-
+    permissions: list["Permissions"] = Relationship(back_populates="roles", link_model=RolePermissions)
