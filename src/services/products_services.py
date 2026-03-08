8b0435e339ba4cdaf206a871c56a4f853ec3df83
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-03-08 18:49:57 +0530

    Removed unnecessary nesting from services

---
+++ src/services/products_services.py
@@ -0,0 +1,21 @@
+from sqlmodel import Session
+from policies.product_policies import ProductPolicy
+from services.rbac.roles_services import fetch_user_and_role
+from db.models.product_model import Products
+from db.models.user_models import Users
+from schemas.product_schemas import ProductRegister
+from exception_handlers.http.http_exceptions import action_not_allowed_exception
+
+def list_product(active_role_dict: dict, product: ProductRegister, session: Session):
+    db_user, role = fetch_user_and_role(active_role_dict, session)
+    policy = ProductPolicy(db_user, role, product, session)
+    if policy.can_list():
+        db_product = Products(**product.model_dump(), seller_id=db_user.id)
+        session.add(db_product)
+        session.flush()
+        session.refresh(db_product)
+        response = db_product.model_dump()
+        response.update({"seller": db_product.seller.username})
+        return response
+    else:
+        raise action_not_allowed_exception
---
+++ src/services/roles_services.py
@@ -0,0 +1,35 @@
+from sqlmodel import Session, select
+from typing import List
+from core.enums import RolesEnum
+from db.models.rbac_models import Roles, UsersRoles
+from db.models.user_models import Users
+from app.auth.security import verify_role, create_access_token
+
+
+def get_all_roles(db_user: Users):
+    roles = [item.role for item in db_user.roles]
+    return roles
+
+
+def create_user_roles(db_user: Users, roles: List[RolesEnum], session: Session):
+    db_user_roles = []
+    for role in roles:
+        roles_query = select(Roles).where(Roles.role == role)
+        db_roles = session.exec(roles_query).one()
+        user_role = {"user": db_user.id, "role": db_roles.id}
+        db_user_role = UsersRoles.model_validate(user_role)
+        db_user_roles.append(db_user_role)
+    return db_user_roles
+
+
+def set_role(db_user: Users, role):
+    verify_role(db_user=db_user, role=role)
+    role_access_token = create_access_token(data={"sub": db_user.username, "role": role})
+    return role_access_token
+
+
+def fetch_user_and_role(active_role_dict, session) -> Users:
+    username = active_role_dict["user"]
+    db_user = session.exec(select(Users).where(Users.username == username)).one()
+    role = active_role_dict["active_role"]
+    return (db_user, role)
